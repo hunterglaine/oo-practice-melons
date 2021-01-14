a89f59ca9082b8ba01e1cda5cc38c8f2ce263975
Hunter Laine <hunterglaine@gmail.com>
2021-01-14 13:08:28 -0800

    Create __init__, add_pairing, and update_code methods in class MelonType

--- harvest.py
+++ harvest.py
@@ -11,28 +11,31 @@ class MelonType(object):
         """Initialize a melon."""
 
         self.pairings = []
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
-        # Fill in the rest
 
-    def add_pairing(self, pairing):
+    def add_pairing(self, *pairing):
         """Add a food pairing to the instance's pairings list."""
 
-        # Fill in the rest
+        self.pairings.extend(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
-        # Fill in the rest
-
+        self.code = new_code
 
 def make_melon_types():
     """Returns a list of current melon types."""
-
-    all_melon_types = []
-
-    # Fill in the rest
-
+    
+    
+    
     return all_melon_types
+
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
